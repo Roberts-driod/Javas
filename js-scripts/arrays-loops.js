a8b821cca0c47911ebf173076af1293cd43c1520
name <male>
2024-11-05 08:53:55 +0200

    Javics-scripts

---
+++ js-scripts/arrays-loops.js
@@ -0,0 +1,16 @@
+
+/* for (i = 11; i <= 21; i++) {
+ console.log(i)
+};
+
+for (i = 21; i >= 11; i--) {
+    console.log(i)
+   }; */
+
+   let draugi = ["bob","dob","beb","boba","biba"];
+   console.log(draugi[0]);
+   console.log(draugi[2]);
+
+   for (i = 0; i < draugi.length; i++) {
+    console.log((i),draugi[i])
+   };
